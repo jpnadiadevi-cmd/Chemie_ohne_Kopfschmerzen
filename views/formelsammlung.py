967e9b82809a60ef79d6f2ed21851492fb8a6cb7
Giuliana Gross <grossgiu@students.zhaw.ch>
2026-04-21 12:26:28 +0200

    formeln

--- views/formelsammlung.py
+++ views/formelsammlung.py
@@ -1,4 +1,17 @@
 import streamlit as st
 
-st.title("Titel der Seite")
-st.write("Hier kommt der Inhalt hin.")
+st.set_page_config(page_title="Formelsammlung", layout="wide")
+
+st.title("📚 Formelsammlung")
+
+st.markdown("---")
+
+# Navigation zu den Unterseiten
+if st.button("🧪 Konzentrationen und Teilchen", use_container_width=True):
+    st.switch_page("views/formelsammlung/konzentrationen_teilchen.py")
+
+if st.button("🧬 Die Molformel", use_container_width=True):
+    st.switch_page("views/formelsammlung/molformel.py")
+
+if st.button("⚗️ Die molare Masse mit PSE", use_container_width=True):
+    st.switch_page("views/formelsammlung/molare_masse.py")
