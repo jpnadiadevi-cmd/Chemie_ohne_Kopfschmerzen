852f32696813f5e1401fe921a5bdcfe5b46d94c9
Giuliana Gross <grossgiu@students.zhaw.ch>
2026-05-15 10:56:40 +0200

    verbessert app.pyx

--- app.py
+++ app.py
@@ -4,18 +4,22 @@ import pandas as pd
 from utils.data_manager import DataManager
 from utils.login_manager import LoginManager
 
-# DataManager und Login initialisieren
-data_manager = DataManager(
-    fs_protocol='webdav',
-    fs_root_folder="Chemie_Informatik2"
-)
+data_manager = DataManager(       # initialize data manager
+    fs_protocol='webdav',         # protocol for the filesystem, use webdav for switch drive
+    fs_root_folder="BMLD_App_DB"  # folder on switch drive where the data is stored
+    ) 
+login_manager = LoginManager(data_manager) # handles user login and registration
+login_manager.login_register()             # stops if not logged in
+# --- END OF NEW CODE ---
 
-login_manager = LoginManager(data_manager)
-login_manager.login_register()
-
-# -------------------------
-# SESSION STATE INITIALISIEREN
-# -------------------------
+# --- CODE UPDATE: load user data from data manager if not already present in session state --
+if 'data_df' not in st.session_state:
+    st.session_state['data_df'] = data_manager.load_user_data(
+        'data.csv',                     # The file on switch drive where the data is stored
+        initial_value=pd.DataFrame(),   # Initial value if the file does not exist
+        parse_dates=['timestamp']       # Parse timestamp as datetime
+    )
+# --- END OF CODE UPDATE ---
 
 if "logbuch_daten" not in st.session_state:
     st.session_state.logbuch_daten = {
